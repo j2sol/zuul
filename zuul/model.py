16b9319c0d609ff76b0dbee1d4d533ac1d3214ce
Gregory Haynes <greg@greghaynes.net>
2015-04-22 20:38:06 -0700

    Support GitHub PR webhooks
    Story: 2000774
    
    Change-Id: I2713c5d19326213539689e9d822831a393b2bf19
    Co-Authored-By: Wayne Warren <waynr+launchpad@sdf.org>
    Co-Authored-By: Jan Hruban <jan.hruban@gooddata.com>
    Co-Authored-By: Jesse Keating <omgjlk@us.ibm.com>

--- zuul/model.py
+++ zuul/model.py
@@ -1767,7 +1767,7 @@ class Change(Ref):
     def getBasePath(self):
         if hasattr(self, 'refspec'):
             return "%s/%s/%s" % (
-                self.number[-2:], self.number, self.patchset)
+                str(self.number)[-2:], self.number, self.patchset)
         return super(Change, self).getBasePath()
 
     def equals(self, other):
@@ -1802,6 +1802,20 @@ class Change(Ref):
         return Attributes(project=self.project,
                           number=self.number,
                           patchset=self.patchset)
+
+
+class PullRequest(Change):
+    def __init__(self, project):
+        super(PullRequest, self).__init__(project)
+        self.updated_at = None
+
+    def isUpdateOf(self, other):
+        if (hasattr(other, 'number') and self.number == other.number and
+            hasattr(other, 'patchset') and self.patchset != other.patchset and
+            hasattr(other, 'updated_at') and
+            self.updated_at > other.updated_at):
+            return True
+        return False
 
 
 class TriggerEvent(object):
