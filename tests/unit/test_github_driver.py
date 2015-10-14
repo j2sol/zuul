8f4688592bd5850587559c366dff89e11bf8ba9e
Jan Hruban <jan.hruban@gooddata.com>
2015-10-14 15:29:33 +0200

    Add 'pr-comment' github webhook event
    Change-Id: I60aa77461cc4f5642ab3da14570f6e7ac3bead1a

--- tests/unit/test_github_driver.py
+++ tests/unit/test_github_driver.py
@@ -55,6 +55,18 @@ class TestGithubDriver(ZuulTestCase):
         self.assertEqual(pr.head_sha, zuulvars['patchset'])
         self.assertEqual(1, len(pr.comments))
 
+    def test_comment_event(self):
+        pr = self.fake_github.openFakePullRequest('org/project', 'master')
+        self.fake_github.emitEvent(pr.getCommentAddedEvent('test me'))
+        self.waitUntilSettled()
+        self.assertEqual(2, len(self.history))
+
+    def test_comment_unmatched_event(self):
+        pr = self.fake_github.openFakePullRequest('org/project', 'master')
+        self.fake_github.emitEvent(pr.getCommentAddedEvent('casual comment'))
+        self.waitUntilSettled()
+        self.assertEqual(0, len(self.history))
+
     def test_tag_event(self):
         self.launch_server.hold_jobs_in_build = True
 
