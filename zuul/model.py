f60b909d68caebc906cd4f595f1c7f216a969982
Jesse Keating <omgjlk@us.ibm.com>
2017-05-04 16:08:58 -0700

    Implement github trigger requirement status
    This allows using a connection specific requirement on the status of the
    head of a PR. A list of statuses is accepted, just like the pipeline
    requirement.
    
    Change-Id: Ib91f6527bf1d8ff5fbc6434c8adaca1cd5e1ba6d

--- zuul/model.py
+++ zuul/model.py
@@ -2068,10 +2068,11 @@ class EventFilter(BaseFilter):
                  event_approvals={}, comments=[], emails=[], usernames=[],
                  timespecs=[], required_approvals=[], reject_approvals=[],
                  pipelines=[], actions=[], labels=[], unlabels=[], states=[],
-                 event_statuses=[], ignore_deletes=True):
+                 statuses=[], event_statuses=[], ignore_deletes=True):
         super(EventFilter, self).__init__(
             required_approvals=required_approvals,
-            reject_approvals=reject_approvals)
+            reject_approvals=reject_approvals,
+            statuses=statuses)
         self.trigger = trigger
         self._types = types
         self._branches = branches
@@ -2134,6 +2135,8 @@ class EventFilter(BaseFilter):
             ret += ' unlabels: %s' % ', '.join(self.unlabels)
         if self.states:
             ret += ' states: %s' % ', '.join(self.states)
+        if self.statuses:
+            ret += ' statuses: %s' % ', '.join(self.statuses)
         if self.event_statuses:
             ret += ' event_statuses: %s' % ', '.join(self.event_statuses)
         ret += '>'
@@ -2256,6 +2259,10 @@ class EventFilter(BaseFilter):
         if self.event_statuses:
             if event.event_status not in self.event_statuses:
                 return False
+
+        # See if the trigger has required statuses
+        if not self.matchesStatuses(change):
+            return False
 
         return True
 
